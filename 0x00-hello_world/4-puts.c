6991b37d86be4534ff6ac305282411d55681ab34
queenola <rukayattkareem@gmail.com>
2023-04-21 06:08:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#!/binbash
 #include <stdio.h>
 /**
- * main - 4-puts.c
- * description - Write a C program that prints exactly
- *
- * Return: Always 0 (Success)
+ * main - Write a C program that prints a line with put function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
