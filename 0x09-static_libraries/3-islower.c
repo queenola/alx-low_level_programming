00803c487da28208da356a47792a0953f9fb1274
queenola <rukayattkareem@gmail.com>
2023-05-17 12:21:30 -0700

    mandatory task

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,16 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-
-int _isupper(int c)
+int _islower(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+	return (c >= 'a' && c <= 'z');
 }
